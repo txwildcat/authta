d3a75d369d17c3abfb5cbaab737f3429ccd218fa
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-09 03:02:04 +0000

    just completed the encyption stuff

--- scripts/encryption.rb
+++ scripts/encryption.rb
@@ -4,5 +4,17 @@
 
 # TODO:
 # 1. encrypt a secret and puts encrypted string
+secret = "tacos"
+puts secret
+
+encrypted_secret = BCrypt::Password.create(secret)
+puts encrypted_secret
+
 # 2. decrypt the encrypted string
+secret_to_test = "tacos"
+decrypted = BCrypt::Password.new(encrypted_secret)
+# puts "decrypted??? #{decrypted}"
+result = BCrypt::Password.new(encrypted_secret) == secret_to_test
+puts result
+
 # 3. check if decrypted value matches secret
