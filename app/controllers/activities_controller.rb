10a430cd8759c9d6c91080dcbb26838ee0898b3b
txwildcat <tyler.askren@kellogg.northwestern.edu>
2022-03-10 00:30:13 +0000

    getting along alright

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
   def create
     @activity = Activity.new(params["activity"])
+    @activity.user_id = @current_user.id # this creates the associations that are required
     @activity.save
     redirect_to "/contacts/#{@activity.contact_id}"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,10 @@ class SessionsController < ApplicationController
         # find the user trying to login
         @user = User.where({email: params["email"]})[0]
         if @user
-            if @user.password == params["password"] #this checks for password accuracy
+            if BCrypt::Password.new(@user.password) #this checks for password accuracy and matches users_controller.rb
+                #cookies: {}
+                #session: {}
+                session["user_id"] = @user.id #this carries the user id to every page by way of storing the cookie
                 flash[:notice] = "You logged in!" #this provides a message
                 redirect_to "/companies"
             else
@@ -18,5 +21,11 @@ class SessionsController < ApplicationController
             flash[:notice] = "Nope."
             redirect_to "/sessions/new"
         end
+    end
+
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "Goodbye."
+        redirect_to "/sessions/new"
     end
 end
